471399a29fb3ac29759a1a392d0b51d231198ec1
gangmuk <gangmuk@gmail.com>
2024-12-15 17:22:18 -0600

    seems working...

---
+++ _plugins/photo_generator.rb
@@ -0,0 +1,20 @@
+module Jekyll
+    class PhotoGenerator < Generator
+      safe true
+      priority :high
+  
+      def generate(site)
+        photo_dir = File.join(site.source, 'assets', 'img', 'photos')
+        photos = Dir.glob("#{photo_dir}/*.{jpg,png,gif,jpeg}").map do |file|
+          {
+            'src' => "/#{file.sub(site.source + '/', '')}",
+            'alt' => File.basename(file, File.extname(file)).capitalize,
+            'title' => File.basename(file, File.extname(file)).capitalize,
+            'description' => "Description for #{File.basename(file, File.extname(file)).capitalize}."
+          }
+        end
+        site.data['photos'] = photos
+      end
+    end
+  end
+  

b55ba2e6440e9c29266d97e5490fb8533892a14d
gangmuk <gangmuk@gmail.com>
2024-12-16 09:08:42 -0600

    update pathh again

--- _plugins/photo_generator.rb
+++ _plugins/photo_generator.rb
@@ -1,19 +1,19 @@
 module Jekyll
-    class PhotoGenerator < Generator
-      safe true
-      priority :high
-  
-      def generate(site)
-        photo_dir = File.join(site.source, 'assets', 'img', 'photos')
-        photos = Dir.glob("#{photo_dir}/*.{jpg,png,gif,jpeg}").map do |file|
-          {
-            'src' => "#{site.baseurl}#{site.config['photourl']}/#{File.basename(file)}",
-            'alt' => File.basename(file, File.extname(file)).capitalize,
-            'title' => File.basename(file, File.extname(file)).capitalize,
-          }
-        end
-        site.data['photos'] = photos
+  class PhotoGenerator < Generator
+    safe true
+    priority :high
+    
+    def generate(site)
+      photo_dir = File.join(site.source, 'assets', 'img', 'photos')
+      photos = Dir.glob("#{photo_dir}/*.{jpg,png,gif,jpeg}").map do |file|
+        # Use relative path from the root of the site
+        {
+          'src' => File.join(site.baseurl, 'assets', 'img', 'photos', File.basename(file)),
+          'alt' => File.basename(file, File.extname(file)).capitalize,
+          'title' => File.basename(file, File.extname(file)).capitalize,
+        }
       end
+      site.data['photos'] = photos
     end
   end
-  
+end

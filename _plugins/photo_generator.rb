8d8b1ce485116fda788e17680fcee9dd51265d7c
gangmuk <gangmuk@gmail.com>
2024-12-16 12:03:52 -0600

    trying absolute path

--- _plugins/photo_generator.rb
+++ _plugins/photo_generator.rb
@@ -7,12 +7,18 @@ module Jekyll
       photo_dir = File.join(site.source, 'assets', 'img', 'photos')
       photos = Dir.glob("#{photo_dir}/*.{jpg,png,gif,jpeg}").map do |file|
         # Use relative path from the root of the site
+        absolute_url = "#{site.config['url']}#{site.config['baseurl']}/assets/img/photos/#{File.basename(file)}"
+        # puts "Found photo: #{file}" # Debug line
+        puts "Generated URL: #{absolute_url}" # Debug line
         {
-          'src' => File.join(site.baseurl, 'assets', 'img', 'photos', File.basename(file)),
+          # 'src' => File.join(site.baseurl, 'assets', 'img', 'photos', File.basename(file)),
+          # 'src' => absolute_url,
+          'src' => "https://gangmuk.github.io/assets/img/photos/#{filename}",
           'alt' => File.basename(file, File.extname(file)).capitalize,
           'title' => File.basename(file, File.extname(file)).capitalize,
         }
       end
+      puts "Total photos found: #{photos.length}" # Debug line
       site.data['photos'] = photos
     end
   end

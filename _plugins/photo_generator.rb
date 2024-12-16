15c265029ad19760c28842c39ac1667f86fd034c
gangmuk <gangmuk@gmail.com>
2024-12-16 12:30:33 -0600

    trying new absolute path for all photots

--- _plugins/photo_generator.rb
+++ _plugins/photo_generator.rb
@@ -6,20 +6,43 @@ module Jekyll
     def generate(site)
       photo_dir = File.join(site.source, 'assets', 'img', 'photos')
       photos = Dir.glob("#{photo_dir}/*.{jpg,png,gif,jpeg}").map do |file|
-        # Use relative path from the root of the site
-        absolute_url = "#{site.config['url']}#{site.config['baseurl']}/assets/img/photos/#{File.basename(file)}"
-        # puts "Found photo: #{file}" # Debug line
-        puts "Generated URL: #{absolute_url}" # Debug line
+        filename = File.basename(file)
+        # Use the full URL that we know works
         {
-          # 'src' => File.join(site.baseurl, 'assets', 'img', 'photos', File.basename(file)),
-          # 'src' => absolute_url,
           'src' => "https://gangmuk.github.io/assets/img/photos/#{filename}",
           'alt' => File.basename(file, File.extname(file)).capitalize,
           'title' => File.basename(file, File.extname(file)).capitalize,
         }
       end
-      puts "Total photos found: #{photos.length}" # Debug line
       site.data['photos'] = photos
     end
   end
 end
+
+
+
+# module Jekyll
+#   class PhotoGenerator < Generator
+#     safe true
+#     priority :high
+    
+#     def generate(site)
+#       photo_dir = File.join(site.source, 'assets', 'img', 'photos')
+#       photos = Dir.glob("#{photo_dir}/*.{jpg,png,gif,jpeg}").map do |file|
+#         # Use relative path from the root of the site
+#         absolute_url = "#{site.config['url']}#{site.config['baseurl']}/assets/img/photos/#{File.basename(file)}"
+#         # puts "Found photo: #{file}" # Debug line
+#         puts "Generated URL: #{absolute_url}" # Debug line
+#         {
+#           # 'src' => File.join(site.baseurl, 'assets', 'img', 'photos', File.basename(file)),
+#           # 'src' => absolute_url,
+#           'src' => "https://gangmuk.github.io/assets/img/photos/#{filename}",
+#           'alt' => File.basename(file, File.extname(file)).capitalize,
+#           'title' => File.basename(file, File.extname(file)).capitalize,
+#         }
+#       end
+#       puts "Total photos found: #{photos.length}" # Debug line
+#       site.data['photos'] = photos
+#     end
+#   end
+# end
